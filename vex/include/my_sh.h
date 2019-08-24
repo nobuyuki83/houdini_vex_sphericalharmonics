611d1417fd4701fa73c1c091ef681297ba62901c
Nobuyuki Umetani <n.umetani@gmail.com>
2019-08-24 20:16:01 +0900

    add license

--- vex/include/my_sh.h
+++ vex/include/my_sh.h
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2019 Nobuyuki Umetani
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+
+
+
 #include "complex.h"
 
 float[] my_sh
